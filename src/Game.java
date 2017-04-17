88b352ceb7b574360d44870ff994a1e85b41f1f7
hugoLucas <hugojlucas@gmail.com>
2017-04-17 12:30:40 -0700

    IR code for links added

--- src/Game.java
+++ src/Game.java
@@ -3,25 +3,49 @@ import java.util.HashMap;
 
 public class Game {
 
+    /* String = "node_n" where n is the number of the node, identifier in intermediate code
+    *  Node = reference to node that has this identifier to intermediate code */
+    private HashMap<String, Node> gameNodes;
 
+    /* A history of nodes traversed thus far */
+    private HashMap<String, String> currentGamePath;
 
-    public class Node{
+    public Game(){
+        gameNodes = new HashMap<>();
+    }
+
+    public void addNodesToGame(Node newNode, String nodeIdentifier){
+        gameNodes.put(nodeIdentifier, newNode);
+    }
+
+    public void addLinkToGame(String nodeIntermediateIdentifier, String nodeOptionIdentifier,
+                              String nodeSourceIntermediateIdentifier){
+        gameNodes.get(nodeIntermediateIdentifier).addLink(nodeOptionIdentifier, nodeSourceIntermediateIdentifier);
+    }
+
+    public static class Node{
         private String nodeIdentifier;
         private String nodeTextPrompt;
 
+        /* <OptionIdentifier, OptionTextPrompt> */
         private HashMap<String, String> optionMap;
-        private ArrayList<Node> nodeLinks;
+
+        /* <OptionIdentifier, NodeIdentifier> */
+        private HashMap<String, String> nodeLinks;
 
         public Node(String id, String text){
             this.nodeIdentifier = id;
             this.nodeTextPrompt = text;
 
             this.optionMap = new HashMap<>();
-            this.nodeLinks = new ArrayList<>();
         }
 
         public void addOption(String id, String text){
             this.optionMap.put(id, text);
+        }
+
+        public void addLink(String opId, String nodeId){
+            this.nodeLinks.put(opId, nodeId);
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,31 @@
  */
 public class Main {
     public static void main(String [] args){
+        Game currentGame = new Game();
+
+
+        Game.Node node_0 = new Game.Node("$pg_five","you pack your wagon");
+        node_0.addOption("$e", "head east");
+        currentGame.addNodesToGame(node_0, "node_0");
+        Game.Node node_1 = new Game.Node("$pg_one","you are in a library");
+        node_1.addOption("$a", "do nothing");
+        node_1.addOption("$b", "check pockets");
+        currentGame.addNodesToGame(node_1, "node_1");
+        Game.Node node_2 = new Game.Node("$pg_four","the library catches on fire");
+        currentGame.addNodesToGame(node_2, "node_2");
+        Game.Node node_3 = new Game.Node("$pg_six","you have caught dysentery");
+        currentGame.addNodesToGame(node_3, "node_3");
+        Game.Node node_4 = new Game.Node("$pg_two","there is a sound in the distance");
+        node_4.addOption("$c", "head towards the sound");
+        currentGame.addNodesToGame(node_4, "node_4");
+        Game.Node node_5 = new Game.Node("$pg_three","you find a digital watch");
+        node_5.addOption("$d", "check the time");
+        currentGame.addNodesToGame(node_5, "node_5");
+        currentGame.addLinkToGame("node_0", "$e", "node_3");
+        currentGame.addLinkToGame("node_1", "$a", "node_4");
+        currentGame.addLinkToGame("node_1", "$b", "node_5");
+        currentGame.addLinkToGame("node_4", "$c", "node_2");
+        currentGame.addLinkToGame("node_5", "$d", "node_2");
 
     }
 }
